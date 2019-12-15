fadf467cfa005812b337ecb623587a2d46a72901
MohammadMahdi <mahdialvansaz@gmail.com>
2019-12-15 21:40:19 +0330

    getppid and getChildren add to defs.h.

--- defs.h
+++ defs.h
@@ -120,6 +120,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
+void            getppid(int);
+int             getChildren(int);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
